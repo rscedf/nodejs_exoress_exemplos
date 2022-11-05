5a33d8a53a7159ba7812c834195bc4d1c965fddd
Rafael Cardoso <rscedf@gmail.com>
2022-11-05 10:28:16 -0300

    criado rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-const express = require('express')
+import * as express from 'express'
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send('Hello World')
+    res.send('GET /')
+})
+
+app.post('/', (req, res)=>{
+    res.status(201).send('POST /')
+})
+
+app.put('/', (req, res)=>{
+    res.status(400).send('PUT /')
+})
+
+app.patch('/', (req, res)=>{
+    res.send('PATCH /')
+})
+
+app.delete('/', (req, res)=>{
+    res.send('DELETE /')
 })
 
 app.listen(3000, ()=> console.log(`ONLINE htpp://localhost:3000`))
